27119effa3e84b025abbd0eb6e8cfb2e2e2c8ed8
talamortis <talamortis@gmail.com>
2018-11-21 02:17:50 +0000

    Made the checks DB driven

--- src/system_censure.cpp
+++ src/system_censure.cpp
@@ -1,10 +1,20 @@
 #include "ScriptMgr.h"
 #include "Chat.h"
+#include <list>
+#include "Containers.h"
+
+std::vector<string>chat;
 
 class SystemCensure : public PlayerScript
 {
 public:
     SystemCensure() : PlayerScript("SystemCensure") {}
+
+    void OnLogin(Player* p)
+    {
+
+        ChatHandler(p->GetSession()).PSendSysMessage("This server is running the |cff4CFF00Chat Censure |rmodule.");
+    }
 
     void OnChat(Player* player, uint32 /*type*/, uint32 lang, std::string& msg)
     {
@@ -33,96 +43,179 @@ public:
 
     void CheckMessage(Player* player, std::string& msg, uint32 lang, Player* /*receiver*/, Group* /*group*/, Guild* /*guild*/, Channel* channel)
     {
-        //if (player->isGameMaster() || lang == LANG_ADDON)
-        //return;
+        //if account is game master let them say what ever they like just incase they need to send the website
+        if (player->GetSession()->GetSecurity() >= 1)
+            return;
 
         // transform to lowercase (for simpler checking)
         std::string lower = msg;
         std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
 
-        const uint8 cheksSize = 67;
-        std::string checks[cheksSize];
-        checks[1] ="http://";
-        checks[0] ="sytes";
-        checks[1] ="http://";
-        checks[2] ="https://";
-        checks[3] ="h t t p : / /";
-        checks[4] ="h t t p s : / /";
-        checks[5] =".com";
-        checks[6] =".net";
-        checks[7] =".org";
-        checks[8] ="www.";
-        checks[9] ="wow-";
-        checks[10] ="-wow";
-        checks[11] ="no-ip";
-        checks[12] =".zapto";
-        checks[13] =".biz";
-        checks[14] =".servegame";
-        checks[15] =".br";
-        checks[16] ="com.br";
-        checks[17] =". c o m";
-        checks[18] =". n e t";
-        checks[19] =". o r g";
-        checks[20] ="w w w .";
-        checks[21] =" w o w -";
-        checks[22] ="- w o w";
-        checks[23] ="n o - i p";
-        checks[24] =". z a p t o";
-        checks[25] =". b i z";
-        checks[26] =". s e r v e g a m e";
-        checks[27] =". b r";
-        checks[28] ="c o m . b r";
-        checks[29] ="h  t  t  p  :  /  /";
-        checks[30] =".  c  o  m";
-        checks[31] =".  n  e  t";
-        checks[32] =".  o  r  g";
-        checks[33] ="w  w  w  .";
-        checks[34] =" w  o  w  -";
-        checks[35] ="-  w  o  w";
-        checks[36] ="n  o  -  i  p";
-        checks[37] =".  z  a  p  t  o";
-        checks[38] =".  b  i  z";
-        checks[39] =".  s  e  r  v  e  g  a  m  e";
-        checks[40] =".  b  r";
-        checks[41] ="c  o  m  .  b  r";
-        checks[42] ="h   t   t   p   :   /   /";
-        checks[43] =".   c   o   m";
-        checks[44] =".   n   e   t";
-        checks[45] =".   o   r   g";
-        checks[46] ="w   w   w   .";
-        checks[47] =" w   o   w   -";
-        checks[48] ="-   w   o   w";
-        checks[49] ="n   o   -   i   p";
-        checks[50] =".   z   a   p   t   o";
-        checks[51] =".   b   i   z";
-        checks[52] =".   s   e   r   v   e   g   a   m   e";
-        checks[53] =".   b   r";
-        checks[54] ="   c   o   m   .   b   r";
-        checks[55] ="h    t    t    p   :   /   /";
-        checks[56] =".    c    o    m";
-        checks[57] =".    n    e   t";
-        checks[58] =".    o    r    g";
-        checks[59] ="w    w    w    .";
-        checks[60] ="w    o    w    -";
-        checks[61] ="-    w    o    w";
-        checks[62] ="n    o    -    i    p";
-        checks[63] =".    z    a    p    t    o";
-        checks[64] =".    b    i     z";
-        checks[65] =".    s    e    r    v    e    g    a    m    e";
-        checks[66] =".    b    r";
-        checks[67] ="c    o    m    .    b    r";
-
-        for (int i = 0; i < cheksSize; ++i)
-        if (lower.find(checks[i]) != std::string::npos)
+        for (int i = 0; i < chat.size(); ++i)
+        if (lower.find(chat[i]) != std::string::npos)
         {
             msg = "";
-            ChatHandler(player->GetSession()).PSendSysMessage("|cffe84118Links/Advertisements are not allowed!"); 
+            ChatHandler(player->GetSession()).PSendSysMessage("Links/Advertisements are not allowed!");
+            return;
+        } 
+    }
+};
+
+class LoadChatTable : public WorldScript
+{
+public:
+    LoadChatTable() : WorldScript("load_system_censure") { }
+
+    void OnLoadCustomDatabaseTable()
+    {
+        sLog->outString("Loading Chat Censure...");
+
+        QueryResult result = CharacterDatabase.PQuery("SELECT `id`,`text` FROM chat_censure");
+
+        if (!result)
+        {
+            sLog->outErrorDb(">>  Loaded 0 Chat Censures. DB table `Chat_Censure` is empty!");
+            sLog->outString();
             return;
         }
+
+        uint32 count = 0;
+        uint32 oldMSTime = getMSTime();
+        
+        do
+        {
+            Field* field = result->Fetch();
+            uint8 id = field[0].GetUInt8();
+            chat.push_back(field[1].GetString());
+
+            count++;
+
+        } while (result->NextRow());
+
+        sLog->outString(">> Loaded %u chat_censure in %u ms", count, GetMSTimeDiffToNow(oldMSTime));
+        sLog->outString("");
     }
+};
+
+class ChatCensureCommand : public CommandScript
+{
+public:
+    ChatCensureCommand() : CommandScript("ChatCensureCommand") { }
+
+    std::vector<ChatCommand> GetCommands() const override
+    {
+        static std::vector<ChatCommand> ChatCensureCommandTable =
+        {
+            { "Reload", SEC_ADMINISTRATOR, false, &HandleReloadCommand, "Realod the chat Censure table" },
+            { "Add",    SEC_ADMINISTRATOR, false, &HandleAddCommand,    "Ban a word. Please use quotation marks when adding."},
+            { "Delete", SEC_ADMINISTRATOR, false, &HandleDeleteCommand, "Delete a banned word. Please use quotation marks when deleting"}
+        };
+
+        static std::vector<ChatCommand> ChatCensurePBaseTable =
+        {
+            { "ChatCensure", SEC_ADMINISTRATOR, false, nullptr, "", ChatCensureCommandTable }
+        };
+
+        return ChatCensurePBaseTable;
+    }
+
+    static bool HandleReloadCommand(ChatHandler* handler, char const* args)
+    {
+        Player* me = handler->GetSession()->GetPlayer();
+
+        if (!me)
+            return false;
+
+        chat.clear();
+        QueryResult result = CharacterDatabase.PQuery("SELECT `id`,`text` FROM chat_censure");
+        uint32 count = 0;
+        uint32 oldMSTime = getMSTime();
+
+        do
+        {
+            Field* field = result->Fetch();
+            uint8 id = field[0].GetUInt8();
+            chat.push_back(field[1].GetString());
+
+            count++;
+
+        } while (result->NextRow());
+
+        ChatHandler(me->GetSession()).PSendSysMessage("Reloaded %u chat censure in %u ms", count, GetMSTimeDiffToNow(oldMSTime));
+        return true;
+
+    }
+
+    static bool HandleAddCommand(ChatHandler* handler, char const* args)
+    {
+        Player* me = handler->GetSession()->GetPlayer();
+
+        if (!me)
+            return false;
+
+        if (!args)
+            return false;
+
+        char* textExtracted = handler->extractQuotedArg((char*)args);
+
+        if (!textExtracted)
+            return false;
+
+        std::string text = textExtracted;
+
+        //lets check the Database to see if arguement already exist
+        QueryResult result = CharacterDatabase.PQuery("SELECT `text` FROM `chat_censure` WHERE `text` = '%s'", text.c_str());
+
+        if (result)
+        {
+            ChatHandler(me->GetSession()).PSendSysMessage("Duplicate text entry for text: |cff4CFF00 %s|r", text.c_str());
+        }
+        else
+        {
+            CharacterDatabase.PQuery("INSERT INTO `chat_censure` (`text`) VALUES ('%s')", text.c_str());
+            ChatHandler(me->GetSession()).PSendSysMessage("Added the text: |cff4CFF00 %s|r to chat censure", text.c_str());
+        }
+        return true;
+    }
+
+    static bool HandleDeleteCommand(ChatHandler* handler, char const* args)
+    {
+        Player* me = handler->GetSession()->GetPlayer();
+
+        if (!me)
+            return false;
+
+        if (!args)
+            return false;
+
+        //we want argument to be in "" for spaces ect.
+        char* textExtracted = handler->extractQuotedArg((char*)args);
+
+        if (!textExtracted)
+            return false;
+
+        std::string text = textExtracted;
+
+        QueryResult result = CharacterDatabase.PQuery("SELECT `text` FROM `chat_censure` WHERE `text` = '%s'", text.c_str());
+
+        if (!result)
+        {
+            ChatHandler(me->GetSession()).PSendSysMessage("Could not find text: |cff4CFF00 %s|r in the Database", text.c_str());
+        }
+        else
+        {
+            CharacterDatabase.PQuery("DELETE FROM `chat_censure` WHERE `text` = '%s'", text.c_str());
+            ChatHandler(me->GetSession()).PSendSysMessage("Deleted Text |cff4CFF00 %s|r please reload the table", text.c_str());
+        }
+
+        return true;
+    }
+
 };
 
 void AddSC_SystemCensure()
 {
+    new ChatCensureCommand();
     new SystemCensure();
+    new LoadChatTable();
 }
